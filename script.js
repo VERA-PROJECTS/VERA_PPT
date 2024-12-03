7f56281067e48d7ec85515b5fcce37a09ecd74a5
Shashwat24M <22bcs10035@gmail.com>
2024-12-03 15:04:24 +0530

    ppt webpage

---
+++ script.js
@@ -0,0 +1,33 @@
+let currentSlide = 1;
+const totalSlides = 13;
+
+function nextSlide() {
+  const current = document.getElementById(`slide${currentSlide}`);
+  current.classList.remove("active");
+
+  currentSlide = currentSlide >= totalSlides ? 1 : currentSlide + 1;
+
+  const next = document.getElementById(`slide${currentSlide}`);
+  next.classList.add("active");
+  showSlideNumber();
+}
+
+function prevSlide() {
+  const current = document.getElementById(`slide${currentSlide}`);
+  current.classList.remove("active");
+
+  currentSlide = currentSlide <= 1 ? 1 : currentSlide - 1;
+
+  const prev = document.getElementById(`slide${currentSlide}`);
+  prev.classList.add("active");
+  showSlideNumber();
+}
+
+function showSlideNumber() {
+  const current = document.querySelector(".currentPage"); // Select the element with the class "currentPage"
+  if (current) {
+    current.innerHTML = `${currentSlide} of ${totalSlides}`; // Update its content
+  }
+}
+
+showSlideNumber();
